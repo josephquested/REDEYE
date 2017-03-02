7cf74216255d09bbd96a74f5f174179724ed9356
josephquested <josephquested@gmail.com>
2017-03-02 22:27:54 +1300

    flying robots

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour {
+	Moves moves;
+
+	void Start ()
+	{
+		moves = GetComponent<Moves>();
+	}
+
+	void Update ()
+	{
+		MovementInput();
+	}
+
+	void MovementInput ()
+	{
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+		{
+			moves.ReceiveInput(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		}
+	}
+}
