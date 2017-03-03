5e8e651149345082bf9d227e776c57f83b585512
josephquested <josephquested@gmail.com>
2017-03-03 22:12:50 +1300

    blade work

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Blade : MonoBehaviour {
 	Animator animator;
 	Rigidbody parentRb;
-	// AudioSource audioSource;
+	AudioSource audioSource;
 	// ParticleSystem particles;
 	CameraShake cameraShake;
 
 	bool firing;
 
 	public float thrust;
+	public float thrustPause;
 	public float cooldown;
 
 	void Start ()
 	{
-		// audioSource = GetComponent<AudioSource>();
 		// particles = transform.parent.GetComponentsInChildren<ParticleSystem>()[0];
+		audioSource = GetComponent<AudioSource>();
 		parentRb = transform.parent.gameObject.GetComponent<Rigidbody>();
 		animator = GetComponent<Animator>();
 		cameraShake = transform.parent.GetComponentsInChildren<CameraShake>()[0];
@@ -25,7 +26,6 @@ public class Blade : MonoBehaviour {
 
 	void Update ()
 	{
-		UpdateAudio();
 		UpdateCameraShake();
 		UpdateParticles();
 	}
@@ -47,6 +47,8 @@ public class Blade : MonoBehaviour {
 		print("firing");
 		firing = true;
 		animator.SetTrigger("fire");
+		yield return new WaitForSeconds(thrustPause);
+		audioSource.Play();
 		parentRb.AddForce(transform.parent.forward * thrust);
 		yield return new WaitForSeconds(cooldown);
 		firing = false;
@@ -55,12 +57,6 @@ public class Blade : MonoBehaviour {
 	void UpdateCameraShake ()
 	{
 		// cameraShake.ReceiveInput(heat);
-	}
-
-	void UpdateAudio ()
-	{
-		// audioSource.volume = heat;
-		// audioSource.pitch = heat * 3;
 	}
 
 	void UpdateParticles ()
