b2389a98d402a40dd20a4706d2ca18a74bbfc14a
josephquested <josephquested@gmail.com>
2017-03-10 16:23:21 +1300

    initial movement work

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour {
+	Movement movement;
+
+	void Start ()
+	{
+		movement = GetComponent<Movement>();
+	}
+
+	void FixedUpdate ()
+	{
+		MovementInput();
+	}
+
+	void MovementInput ()
+	{
+		movement.ReceiveInput(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	Rigidbody rb;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void ReceiveInput (float horizontal, float vertical)
+	{
+		Vector3 movement = new Vector3(horizontal, 0, vertical);
+		Move(movement);
+	}
+
+	void Move (Vector3 movement)
+	{
+		rb.AddRelativeForce(movement * speed);
+	}
+}
