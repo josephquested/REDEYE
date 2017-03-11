efddd0d560d81835ac63cce5af749adf147dcff1
josephquested <josephquested@gmail.com>
2017-03-11 13:59:54 +1300

    laser work

---
+++ Assets/Scripts/Halflife.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Halflife : MonoBehaviour {
+	public float secondsAlive;
+
+	void Start ()
+	{
+		Destroy(gameObject, secondsAlive);
+	}
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+	public GameObject tailPrefab;
+	public float cooldown;
+
+	void Start ()
+	{
+		StartCoroutine(TailRoutine());
+	}
+
+	IEnumerator TailRoutine ()
+	{
+		yield return new WaitForSeconds(cooldown);
+		Instantiate(tailPrefab, transform.position, transform.rotation);
+		StartCoroutine(TailRoutine());
+	}
+}
