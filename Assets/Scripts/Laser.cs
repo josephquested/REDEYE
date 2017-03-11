1efc6ee01145e503b93960a9b076fc45f4e11fb9
josephquested <josephquested@gmail.com>
2017-03-11 17:24:38 +1300

    initial laser sizzle

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour {
+	Light laserLight;
+
 	public GameObject tailPrefab;
 	public float cooldown;
+	public float decay;
 
 	void Start ()
 	{
 		StartCoroutine(TailRoutine());
+		laserLight = GetComponentInChildren<Light>();
+		StartCoroutine(FadeRoutine());
 	}
 
 	IEnumerator TailRoutine ()
 	{
-		yield return new WaitForSeconds(cooldown);
 		Instantiate(tailPrefab, transform.position, transform.rotation);
+		yield return new WaitForSeconds(cooldown);
 		StartCoroutine(TailRoutine());
+	}
+
+	IEnumerator FadeRoutine ()
+	{
+		laserLight.range -= decay;
+		yield return new WaitForSeconds(0.001f);
+		StartCoroutine(FadeRoutine());
 	}
 }
