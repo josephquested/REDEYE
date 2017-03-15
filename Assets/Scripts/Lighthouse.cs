5e7e35f4f82898810f21c269d75b70113b52f524
josephquested <josephquested@gmail.com>
2017-03-16 09:19:12 +1300

    initial lighthouse work

---
+++ Assets/Scripts/Lighthouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighthouse : MonoBehaviour {
+	Transform target;
+
+	public GameObject eye;
+
+	void Start ()
+	{
+		target = GameObject.FindWithTag("Player").transform;
+	}
+
+	void Update ()
+	{
+		FacePlayer();
+	}
+
+	void FacePlayer ()
+	{
+
+		Vector3 targetDir = target.position - transform.position;
+    Vector3 newDir = Vector3.RotateTowards(target.position, targetDir, 1, 0.0F);
+    eye.transform.rotation = Quaternion.LookRotation(newDir);
+	}
+}
