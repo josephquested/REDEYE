a30a51657f04e563d892f9885df21b93d57ea784
josephquested <josephquested@gmail.com>
2017-03-16 10:53:48 +1300

    lighthouse can burn you

--- Assets/Scripts/Lighthouse.cs
+++ Assets/Scripts/Lighthouse.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class Lighthouse : MonoBehaviour {
 	Transform target;
+	AudioSource audioSource;
+	bool bakingPlayer;
 
 	public GameObject eye;
 	public float strength;
+	public float bake;
+	public float bakeSpeed;
+	public ParticleSystem bakeParticles;
+	public Light spotLight;
+	public LighthouseAttackTrigger attackTrigger;
 	public bool trackPlayer;
 
 	void Start ()
 	{
 		target = GameObject.FindWithTag("Player").transform;
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	void Update ()
+	{
+		UpdateTracking();
+		UpdateFire();
+		UpdateBake();
+	}
+
+	void UpdateFire ()
+	{
+		if (attackTrigger.heat >= 1)
+		{
+			StartFire();
+		}
+		else
+		{
+			StopFire();
+		}
+	}
+
+	void UpdateTracking ()
 	{
 		if (trackPlayer)
 		{
@@ -24,6 +51,47 @@ public class Lighthouse : MonoBehaviour {
 		{
 			Rotate();
 		}
+	}
+
+	void UpdateBake ()
+	{
+		if (bakingPlayer)
+		{
+			bake += bakeSpeed;
+		}
+		else
+		{
+			bake -= bakeSpeed;
+		}
+		if (bake >= 1)
+		{
+			if (GameObject.FindWithTag("Player") != null)
+			{
+				GameObject.FindWithTag("Player").GetComponent<Status>().Die();
+				attackTrigger.heat = 0;
+				StopFire();
+			}
+		}
+		if (bake < 0)
+		{
+			bake = 0;
+		}
+	}
+
+	void StartFire ()
+	{
+		bakingPlayer = true;
+		bakeParticles.Play();
+		audioSource.Play();
+		spotLight.intensity = 300f;
+	}
+
+	void StopFire ()
+	{
+		bakingPlayer = false;
+		audioSource.Stop();
+		bakeParticles.Stop();
+		spotLight.intensity = 20f;
 	}
 
 	void FacePlayer ()
---
+++ Assets/Scripts/LighthouseAttackTrigger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LighthouseAttackTrigger : MonoBehaviour {
+	AudioSource audioSource;
+
+	public Lighthouse lighthouse;
+	public ParticleSystem heatParticles;
+	public float heatSpeed;
+	public float heat = 0;
+	public bool heating;
+
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update ()
+	{
+		if (heating)
+		{
+			heat += heatSpeed;
+		}
+		else
+		{
+			heat -= heatSpeed;
+		}
+		if (heat < 0)
+		{
+			heat = 0;
+		}
+		if (heat >= 1)
+		{
+			heat = 1;
+		}
+	}
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.tag == "Player")
+		{
+			heating = true;
+			heatParticles.Play();
+			audioSource.Play();
+		}
+	}
+
+	void OnTriggerExit (Collider collider)
+	{
+		if (collider.tag == "Player")
+		{
+			heating = false;
+			heatParticles.Stop();
+			audioSource.Stop();
+		}
+	}
+}
