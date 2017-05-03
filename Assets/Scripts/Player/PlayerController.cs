df991c7661741d041164ac94e271c6685348a0e0
Joseph Quested <josephquested@gmail.com>
2017-05-03 19:01:18 +1200

    initial first level work

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// SYSTEM //
+
+	PlayerMovement playerMovement;
+
+	void Start ()
+	{
+		playerMovement = GetComponent<PlayerMovement>();
+	}
+
+	// MOVEMENT //
+
+	public void ReceiveMovement (float horizontal, float vertical)
+	{
+		if (horizontal != 0 || vertical != 0)
+		{
+			playerMovement.ReceiveMovement(horizontal, vertical);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	// SYSTEM //
+
+	PlayerController playerController;
+
+	void Start ()
+	{
+		playerController = GetComponent<PlayerController>();
+	}
+
+	void Update ()
+	{
+		UpdateMovement();
+	}
+
+	// MOVEMENT //
+
+	void UpdateMovement ()
+	{
+		playerController.ReceiveMovement(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+}
